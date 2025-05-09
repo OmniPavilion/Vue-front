b4b3e3c6ebe1a59d579b61b533834d923ccbe20c
pc2005 <pvc2005303@outlook.com>
2025-05-09 20:01:44 +0800

    axios相关

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,12 +1,7 @@
 import type { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
 import axios from 'axios';
 
-// 定义基础响应数据类型
-interface BaseResponse<T = any> {
-    code: number;
-    message: string;
-    data: T;
-}
+import type {Result} from '@/types/dto/Result'
 
 // 创建 axios 实例
 const myAxios: AxiosInstance = axios.create({
@@ -34,14 +29,14 @@ myAxios.interceptors.request.use(
 
 // 响应拦截器
 myAxios.interceptors.response.use(
-    <T>(response: AxiosResponse<BaseResponse<T>>): T => {
+    <T>(response: AxiosResponse<Result<T>>): AxiosResponse<Result<T>> => {
         if (response.status !== 200) {
             throw new Error(response.data.message || 'Request failed');
         }
         if (response.data.code != 1) {
             throw new Error(response.data.message || 'Request failed');
         }
-        return response.data;
+        return response;
     },
     (error: AxiosError): Promise<AxiosError> => {
         // 统一错误处理
